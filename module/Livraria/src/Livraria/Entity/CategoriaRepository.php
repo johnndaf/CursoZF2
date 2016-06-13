922cbe1305cd995cfc28bcf48238176cd8de9da9
Johnn Amancio <jdamancio@gmail.com>
2016-06-13 16:48:31 -0300

    curso

---
+++ module/Livraria/src/Livraria/Entity/CategoriaRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Livraria\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Description of CategoriaRepository
+ *
+ * @author Johnn
+ */
+class CategoriaRepository extends EntityRepository{
+    //put your code here
+}
--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
+
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
 chdir(dirname(__DIR__));
+
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
